aab1e493c6e12cc2fab43803dffcecad047e5976
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2022-02-07 21:11:44 +0100

    fix - image and homepage layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,7 @@ const Home = () => {
           start: "30% 90%",
           end: "bottom"
         }
-    });
+      });
 
     if (welcome?.current !== undefined) {
       let chars = SplitTextToChars(welcome.current);
@@ -60,24 +60,37 @@ const Home = () => {
 
   return (
     <>
-
-      <img src={process.env.PUBLIC_URL + '/b1.png'} className="bg-image top" />
+      <div id="section1" className="section-img">
+        <img src={process.env.PUBLIC_URL + '/b1.png'} className="bg-image" />
+      </div>
       <div id="section1" className="section">
-        {user.length > 0 && <h2 ref={welcome}>Welcome {user}!</h2> }
-        <p>
-          If you'd like to automate the inclusion of import React from 'react' for all files that use jsx syntax, install the react-require babel plugin:
+        <div className="text-content">
+          {user.length > 0 && <h2 ref={welcome}>Welcome {user}!</h2>}
+          <br></br>
+          <h4>Importing React</h4>
+          <p>
+            If you'd like to automate the inclusion of import React from 'react' for all files that use jsx syntax, install the react-require babel plugin:
 
-          npm install babel-plugin-react-require --save-dev
-          Add react-require into .babelrc.
-          This plugin should be defined before transform-es2015-modules-commonjs plugin because it's using ES2015 modules syntax to import React into scope.
-        </p>
-        <p>
-          SPA single page application działa w ten sposób, że klient wysyła zadanie do serwera,
-          adres w przeglądarce i otrzymuje od serwera tylko zmieniona zawartość (bez nagłówków itp, inncych komponentów).
-          Zawartość jest w pliku js i jest dynamicznie tworzony plik html już po stronie klienta.
-          W momencie gdy chcemy wyświetlić podstronę np /contact klient nie komunikuje się z serwerem,
-          tylko we własnym zakresie zmienia kontent (update DOM)
-        </p>
+            npm install babel-plugin-react-require --save-dev
+            Add react-require into .babelrc.
+            This plugin should be defined before transform-es2015-modules-commonjs plugin because it's using ES2015 modules syntax to import React into scope.
+          </p>
+          <h4>Lorem ipsum</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <h4>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</h4>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+            sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
+            nisi ut aliquid ex ea commodi consequatur?
+            Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </p>
+        </div>
       </div>
       <div id="section2" className="section" ref={promotionsRef} >
         <Promotions />
