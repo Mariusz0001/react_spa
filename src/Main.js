b8787bec01ccc46333e425e2ce4d4e0c875b84c1
Mariusz Pawlus <mariusz.pawlus95@gmail.com>
2023-09-08 21:50:10 +0200

    fix router

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 import Contact from "./components/Contact/Contact";
